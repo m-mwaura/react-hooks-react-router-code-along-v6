96da39b8b23c6ca659d05f74869e6d416931a96f
felix mwangi <fmwauramwangi@gmail.com>
2025-02-04 10:20:11 +0300

    src/routes.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
+import routes from "./routes";
+import { createBrowserRouter } from "react-router-dom";
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Home />);
---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+function About(){
+    return (
+        <>
+        <header>
+
+        </header>
+        <main>
+            <h1>My about component</h1>
+        </main>
+        </>
+    );
+}
+
+export default About;
